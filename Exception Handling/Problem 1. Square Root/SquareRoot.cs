8fb26c839c5156a6d6d59182e12b9a024a1a5ee8
Dimitar <dimitar@dimitrov-bg.info>
2015-06-11 01:00:31 +0300

    Update SquareRoot.cs

--- Exception Handling/Problem 1. Square Root/SquareRoot.cs
+++ Exception Handling/Problem 1. Square Root/SquareRoot.cs
@@ -1,3 +1,9 @@
+/*
+    Write a program that reads an integer number and calculates and prints its square root.
+    If the number is invalid or negative, print "Invalid number". 
+    In all cases finally print "Good bye". Use try-catch-finally.
+*/
+
 ﻿namespace Problem_1.Square_Root
 {
     using System;
