1baee0d4b64beedb88fe5b85c2dcc3e77a06bc20
Dimitar <dimitar@dimitrov-bg.info>
2015-06-13 23:28:56 +0300

    Update SquareRoot.cs

--- Exception Handling/Problem 1. Square Root/SquareRoot.cs
+++ Exception Handling/Problem 1. Square Root/SquareRoot.cs
@@ -14,34 +14,40 @@
         {
             try
             {
+                // GET INPUT FROM CONSOLE
                 string input = Console.ReadLine();
 
+                // IF INPUT IS NULL
                 if (input == null)
                 {
                     throw new NullReferenceException();
                 }
 
+                // PARSE INPUT TO INTEGER NUMBER
                 int number = int.Parse(input);
 
+                // IF THE NUMBER IS SMALLER THAN 1
                 if (number < 1)
                 {
                     throw new ArgumentOutOfRangeException();
                 }
 
+                // PRINT THE SQARE ROOT OF NUMBER
                 Console.WriteLine(Math.Sqrt(number));
             }
+            // CATCH EXCEPTIONS
             catch (System.FormatException)
-            {
+            {   // FORMAT$
                 Console.Error.Write("Expected valid integer number\r\n");
-            }
+            }   // OVERFLOW
             catch (OverflowException)
             {
                 Console.Error.Write("Number was too large, please try again with different/smaller number.\r\n");
-            }
+            }   // OUT OF RANGE
             catch (ArgumentOutOfRangeException)
-            {
+            {   // NEGATIVE NUMBER
                 Console.Error.WriteLine("Number must not be negative.\r\n");
-            }
+            }   // NULL
             catch (NullReferenceException)
             {
                 Console.Error.Write("Cannot calculate square root of NULL.\r\n");
